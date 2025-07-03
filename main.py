7762e731c0f7c4435d7c56d8d9343afabd4ecbc7
Indera Ihsan <inderaihsan@yahoo.com>
2025-07-03 14:28:33 +0700

    update-portfolio

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import os
 
 from st_pages import add_page_title, get_nav_from_toml, hide_pages
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", 
+                   initial_sidebar_state="expanded"
+                )
 
-sections = True
+sections = True 
+
+st.logo(
+    'assets/man.png'
+)
 
 nav = get_nav_from_toml(
     ".streamlit/pages_sections.toml"
